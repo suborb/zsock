891f44aa308b87fd6036d6534c5e5efe3ae921b2
suborb <dom@suborbital.org.uk>
2002-06-08 17:19:03 +0000

    few prettyup bits + copyright

--- ftp/ftp.c
+++ ftp/ftp.c
@@ -1,6 +1,43 @@
+/*
+ * Copyright (c) 2001-2002 Dominic Morris
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Dominic Morris.
+ * 4. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.  
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+ *
+ * This file is part of the ZSock TCP/IP stack.
+ *
+ * $Id: ftp.c,v 1.6 2002-06-08 17:19:03 dom Exp $
+ *
+ */
+
 #include "ftp.h"
 
-/* Put all the data together */
+/* Put all the data together so that it's not paged out by ZSock */
 
 char file_buffer[1024];
 char buffer[160];
@@ -124,6 +161,10 @@ unsigned long sendfile(FILE *fp)
 
     while ( ( s = fread(file_buffer,1,sizeof(file_buffer),fp) ) != 0  ) {
 	net_write(ftpdata_fd,file_buffer,s);
+#ifdef SCCZ80
+	if ( getk() == 27 )
+	    break;
+#endif
 	total += s;
 	if ( hash && total/ 256 > c ) {
 	    i = total / 256 - c;
@@ -145,6 +186,7 @@ unsigned long recvfile(FILE *fp)
     while ( ( s = net_read(ftpdata_fd,file_buffer,sizeof(file_buffer)) ) != -1 ) {
 	if ( s == 0 )
 	    break;
+
 	if ( fp == stdout ) {
 	    file_buffer[s-1] = 0;
 	    print_native();	   
--- ftp/net_zsock.c
+++ ftp/net_zsock.c
@@ -1,3 +1,40 @@
+/*
+ * Copyright (c) 2001-2002 Dominic Morris
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *      This product includes software developed by Dominic Morris.
+ * 4. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.  
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+ *
+ * This file is part of the ZSock TCP/IP stack.
+ *
+ * $Id: net_zsock.c,v 1.4 2002-06-08 17:19:03 dom Exp $
+ *
+ */
+
 #include "ftp.h"
 
 
@@ -259,13 +296,15 @@ int fgets_net(unsigned char *str, int max, int echo)
 	  fputc_cons('\n');
 	  return ptr;
       }
-      if (c == 127 && ptr > 0 ) {
-         str[--ptr] = 0;
-	 if ( echo ) {
-	     fputc_cons(8);
-	     fputc_cons(32);
-	     fputc_cons(8);
-	 }
+      if (c == 127  ) {
+	  if ( ptr > 0 ) {
+	      str[--ptr] = 0;
+	      if ( echo ) {
+		  fputc_cons(8);
+		  fputc_cons(32);
+		  fputc_cons(8);
+	      }
+	  }
       }
       else {
          str[ptr++] = c;
